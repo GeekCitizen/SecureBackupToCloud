522eaa998c97559a5e88e2ed2d6e0688b474d9a8
GeekCitizen <ludovic.desfontaines@gmail.com>
2015-12-01 23:28:38 +0100

    Create Config.pl

---
+++ Config.pl
@@ -0,0 +1,8 @@
+#srcdir		=> ["/Users/ludo/Music/iTunes/iTunes Media/Music", "/Users/ludo/Public/Development/workspace"],
+# srcdir	=> ["/Users/ludo/Music/iTunes/iTunes Media/Music", "/Users/ludo/Public/Development/workspace"],
+#srcdir		=> ["/Users/ludo/Desktop", "/Users/ludo/Documents/script"],
+
+srcdir		=> ["/Users/ludo/Public/Development/workspace", "/Users/ludo/Documents/script"],
+tgtdir		=> '/Users/ludo/Documents/script.dest',
+sqldb		=> "totoa.db",
+dirprefix	=> "SRCDIR-",

95d8f7b0e194ddc2a0cba7f97b116ee48dafa3dd
GeekCitizen <ludovic.desfontaines@gmail.com>
2015-12-10 14:49:39 +0100

    Delete Config.pl

--- Config.pl
+++
@@ -1,8 +0,0 @@
-#srcdir		=> ["/Users/ludo/Music/iTunes/iTunes Media/Music", "/Users/ludo/Public/Development/workspace"],
-# srcdir	=> ["/Users/ludo/Music/iTunes/iTunes Media/Music", "/Users/ludo/Public/Development/workspace"],
-#srcdir		=> ["/Users/ludo/Desktop", "/Users/ludo/Documents/script"],
-
-srcdir		=> ["/Users/ludo/Public/Development/workspace", "/Users/ludo/Documents/script"],
-tgtdir		=> '/Users/ludo/Documents/script.dest',
-sqldb		=> "totoa.db",
-dirprefix	=> "SRCDIR-",
